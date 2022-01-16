43865c6805078537f4ce56fda37e034567a0e6d6
VoChiDanh <vochidanh.hs.c@gmail.com>
2022-01-16 13:55:43 +0700

    Fix event

--- src/main/java/net/danh/gang/Events/Events.java
+++ src/main/java/net/danh/gang/Events/Events.java
@@ -1,13 +1,18 @@
 package net.danh.gang.Events;
 
+import net.danh.gang.Files.Files;
 import net.danh.gang.Manager.Gangs;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
+
+import java.util.List;
 
 public class Events implements Listener {
     @EventHandler
@@ -20,9 +25,12 @@ public class Events implements Listener {
     @EventHandler
     public void onBreak(BlockBreakEvent e) {
         Player p = e.getPlayer();
-        if (Gangs.inGang(p).booleanValue()) {
-            Gangs pGang = Gangs.getGang(p);
-            pGang.addXP(1);
+        List<String> items = Files.getInstance().getconfig().getStringList("Break");
+        if (Gangs.inGang(p)) {
+            if (items.equals(e.getBlock().getType())) {
+                Gangs pGang = Gangs.getGang(p);
+                pGang.addXP(Files.getInstance().getconfig().getInt("Break." + e.getBlock().getType()));
+            }
         }
     }
 }
